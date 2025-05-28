b97684d31992a8059029b6a1a95e2268499438c4
mateooalvarez <mateoalvarezlettieri@gmail.com>
2025-05-28 17:20:44 -0300

    arreglo del role

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const bcrypt = require('bcrypt');
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, unique: true, required: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  role: { type: String, default: 'usuario' }
 });
 
 // Middleware para hashear la password antes de guardar
